26cc3b689de44f0a12a2ba46a54d3bc9147c0bcc
Joe Davis <jdavis@grayshift.com>
2022-06-14 17:10:48 -0500

    adding settings icon

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react';
 import { useNavigate } from '@remix-run/react';
 import { v4 as uuidv4 } from 'uuid';
 import MenuLink from '~/components/MenuLink';
+import svg from '~/assets/settings.svg';
 
 const Layout: FC = ({ children }) => {
   const navigate = useNavigate();
@@ -29,7 +30,10 @@ const Layout: FC = ({ children }) => {
           </MenuLink>
         </div>
         <div className="font-bold text-gray-700">Tools</div>
-        <MenuLink clickHandler={() => navigate(`/solver`)}>Solver</MenuLink>
+        <div className="flex justify-between w-80 mb-5">
+          <MenuLink clickHandler={() => navigate(`/solver`)}>Solver</MenuLink>
+        </div>
+        <img src={svg} className="settings-icon" alt="settings-icon" />
       </div>
       <div className="text-center font-logo text-5xl text-gray-500 mt-10">
         Sudoku
